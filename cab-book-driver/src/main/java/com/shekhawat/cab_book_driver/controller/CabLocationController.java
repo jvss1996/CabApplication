fc3e3e8a0aac563fe803399dc862e871143aea0c
Jaivardhan <jvss1996@gmail.com>
2024-08-18 18:26:28 -0500

    configured kafka properties, sending location to kafka topic on hitting location endpoint

--- cab-book-driver/src/main/java/com/shekhawat/cab_book_driver/controller/CabLocationController.java
+++ cab-book-driver/src/main/java/com/shekhawat/cab_book_driver/controller/CabLocationController.java
@@ -4,13 +4,13 @@ import com.shekhawat.cab_book_driver.service.CabLocationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
-@Controller
+@RestController
 @RequestMapping("/location")
 public class CabLocationController {
 
@@ -18,10 +18,11 @@ public class CabLocationController {
     private CabLocationService cabLocationService;
 
     @PutMapping
-    public ResponseEntity updateLocation() {
-        int range = 100;
-        while (range > 100) {
-            System.out.println(Math.random() + ", " + Math.random());
+    public ResponseEntity updateLocation() throws InterruptedException {
+        int range = 5;
+        while (range > 0) {
+            cabLocationService.updateLocation(Math.random() + ", " + Math.random());
+            Thread.sleep(1000);
             range--;
         }
         return new ResponseEntity<>(Map.of("message", "Location Updated Successfully"), HttpStatus.OK);
--- cab-book-driver/src/main/java/com/shekhawat/cab_book_driver/service/CabLocationService.java
+++ cab-book-driver/src/main/java/com/shekhawat/cab_book_driver/service/CabLocationService.java
@@ -1,4 +1,17 @@
 package com.shekhawat.cab_book_driver.service;
 
+import com.shekhawat.cab_book_driver.constant.AppConstant;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+@Service
 public class CabLocationService {
+
+    @Autowired
+    private KafkaTemplate<String, Object> kafkaTemplate;
+
+    public void updateLocation(String location) {
+        kafkaTemplate.send(AppConstant.CAB_LOCATION, location);
+    }
 }
